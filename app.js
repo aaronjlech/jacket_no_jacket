29cc059ae4b155fe58c086c8f7194ff856b91228
Aaron LeCheminant <aaronjlech@gmail.com>
2018-02-19 10:04:48 -0500

    setup express hello world app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+mongoose.Promise = require('bluebird');
+
+// insert mongo db to use
+// mongoose.connect('mongodb://localhost:27017/jacket');
+
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+
+const port = 5000;
+app.listen(port, () => {
+   //should see these console loggin in the terminal
+   console.log("server is running!");
+   console.log('listening at port: ', port);
+});
